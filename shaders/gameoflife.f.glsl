9a96bd900f58ea8c6ca02f56bf5250a0f9b5a665
Vichoko <vicenteoyanedel@gmail.com>
2017-10-06 00:53:44 -0300

    sghdr fix

--- shaders/gameoflife.f.glsl
+++ shaders/gameoflife.f.glsl
@@ -6,4 +6,4 @@ void main(void) {
   gl_FragColor[0] = ourColor.x;
   gl_FragColor[1] = ourColor.y;
   gl_FragColor[2] = ourColor.z; 
-};
+}
--- shaders/gameoflife.v.glsl
+++ shaders/gameoflife.v.glsl
@@ -8,4 +8,4 @@ varying vec3 ourColor; // output a color to the fragment shader
 void main(void) {                        
   gl_Position = vec4(coord2d, 0.0, 1.0);
   ourColor = color;
-};
+}
